3511df72786c1dfd63f42888fa83bb9a6a4e70ad
CHOCO <jh780465@naver.com>
2024-01-22 10:04:13 +0900

    project setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,9 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
-}
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
