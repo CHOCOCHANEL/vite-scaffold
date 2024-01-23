09c7be00483afd1a476be319b7aea098a6ff2cd8
choco-chanel <jh780465@naver.com>
2024-01-23 17:28:37 +0900

    Tutoirals in Docs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Hello Vite Scaffolding</div>
+  <h1>Declarative Rendering</h1>
+  <h2>The core feature of Vue is declarative rendering</h2>
+  <p>Using a template syntax that extends HTML,</p>
 </template>
 
 <script>
